ce0f6312f08c19f087cee1e0987601f15e592b42
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2017-02-20 13:32:02 +0530

    added osm tiles div

--- sources/script/mapPage.js
+++ sources/script/mapPage.js
@@ -1,4 +1,11 @@
 
 $(document).ready(function() {
-  var map = L.map('mapEmpty').setView([51.505, -0.09], 13);
+  //for displaying empty map
+  var mapEmpty = L.map('mapEmpty').setView([14.477234,78.804932], 13);
+
+
+  //For adding OSM Tile Layer to the Map
+  var mapOSM=L.map('mapOSM').setView([14.477234,78.80492],14);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mapOSM);
+
 });
