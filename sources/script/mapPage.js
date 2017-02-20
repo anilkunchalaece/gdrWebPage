e2c61576f0bb6bab4f3d3f5eb94ea52b646955b9
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2017-02-20 15:31:55 +0530

    added map with popup

--- sources/script/mapPage.js
+++ sources/script/mapPage.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function() {
   //for displaying empty map
-  var mapEmpty = L.map('mapEmpty').setView([14.477234,78.804932], 13);
+  var mapEmpty = L.map('mapEmpty').setView([14.476832,78.765157], 12);
 
 
   //For adding OSM Tile Layer to the Map
-  var mapOSM=L.map('mapOSM').setView([14.477234,78.80492],14);
+  var mapOSM=L.map('mapOSM').setView([14.476832,78.765157],12);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mapOSM);
+
+  //For adding osm map tile layer and popUp
+  var mapWithPopUp = L.map('mapWithPopUp').setView([14.476832,78.765157],12);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mapWithPopUp);
+  var ksrmMarker = L.marker([14.476832,78.765157]).addTo(mapWithPopUp);
+  ksrmMarker.bindPopup('KSRM College of Engineering').openPopup();
+
+  
 
 });
